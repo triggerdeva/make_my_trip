421a49a1090128a97c087c5818e419cc54f94ef2
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2023-08-05 11:09:07 +0530

    done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,8 @@ import logo from "../assets/logo.png"
 import { useNavigate } from 'react-router-dom'
 import {context} from "../App";
 import logoutIcon from "../assets/box-arrow-right.svg";
+import header from "./header";
+
 const Nav = () => {
   const {setData,currentUser,setCurrentUser} = useContext(context);
   const navigate = useNavigate();
@@ -19,9 +21,36 @@ const Nav = () => {
     <div className='navbar'>
         <img onClick={() => navigate("/")} src={logo} id="logo" />
         <ul className='nav-items'>
-            <button onClick={() => handleNavigate("/flights") }>Flights</button>
-            <button onClick={() => handleNavigate("/hotels") }>Stays</button>
-            <button onClick={() => handleNavigate("/trains") }>Trains</button>
+            <button onClick={() => handleNavigate("/flights")}>
+            Flights
+            <img src="/src/assets/plane.png" id="flight"></img>
+            </button>
+
+            <button onClick={() => handleNavigate("/hotels") }>
+            Stays
+            <img src="/src/assets/stays.png" id="stays"></img>
+            </button>
+
+            <button onClick={() => handleNavigate("/trains") }>
+            Trains
+            <img src="/src/assets/vandebharat.png" id="train"></img>
+            </button>
+
+            <button onClick={() => handleNavigate("/trains") }>
+            Bus
+            <img src="/src/assets/bus.png" id="bus"></img>
+            </button>
+
+            <button onClick={() => handleNavigate("/trains") }>
+            Cabs
+            <img src="/src/assets/taxi.png" id="cab"></img>
+            </button>
+
+            <button onClick={() => handleNavigate("/trains") }>
+            Chater Flight
+            <img src="/src/assets/charter flight.png" id="chaterflt"></img>
+            </button>
+
             { currentUser ? <button>Hello, { currentUser.email.substring(0, currentUser.email.indexOf('@'))}</button> : <button onClick={() => navigate("loginSignup","replace")}>Login / Signup</button>}
             { currentUser && <img onClick={logout} src={logoutIcon} alt="logout" />}
         </ul>
